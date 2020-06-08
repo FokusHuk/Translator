aad26deffbe7bcf023efd62277721ed0e6c1d7af
Ilya <ilyaaxnov1999@gmail.com>
2020-06-08 20:01:48 +0300

    update 08.06 add: hash table

---
+++ Translator/HTElement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Translator
+{
+    class HTElement
+    {
+        public int Key { get; private set; }
+        public double Value { get; private set; }
+
+        public HTElement(int key, double value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0}: {1}", Key, Value);
+        }
+    }
+}
---
+++ Translator/HashTable.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace Translator
+{
+    class HashTable
+    {
+        private readonly byte tableSize = 255;
+
+        private Dictionary<int, List<HTElement>> _items = null;
+
+        public ReadOnlyCollection<KeyValuePair<int, List<HTElement>>> Items => _items?.ToList()?.AsReadOnly();
+
+        public HashTable()
+        {
+            _items = new Dictionary<int, List<HTElement>>(tableSize);
+        }
+        
+        public void insert(int key, double value)
+        {
+            HTElement newItem = new HTElement(key, value);
+
+            int hash = GetHash(newItem.Key);
+
+            List<HTElement> hashTableItem = null;
+
+            if (_items.ContainsKey(hash))
+            {
+                hashTableItem = _items[hash];
+
+                HTElement oldElementWithKey = hashTableItem.SingleOrDefault(i => i.Key == newItem.Key);
+
+                if (oldElementWithKey != null)
+                {
+                    throw new ArgumentException($"Хеш-таблица уже содержит элемент с ключом {key}. Ключ должен быть уникален.", nameof(key));
+                }
+
+                _items[hash].Add(newItem);
+            }
+            else
+            {
+                hashTableItem = new List<HTElement> { newItem };
+
+                _items.Add(hash, hashTableItem);
+            }
+        }
+
+        public void delete(int key)
+        {
+            int hash = GetHash(key);
+
+            if (!_items.ContainsKey(hash))
+            {
+                return;
+            }
+
+            var hashTableItem = _items[hash];
+
+            HTElement item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+            if (item != null)
+            {
+                hashTableItem.Remove(item);
+
+                if (hashTableItem.Count == 0)
+                    _items.Remove(hash);
+            }
+        }
+
+        public double search(int key)
+        {
+            int hash = GetHash(key);
+
+            if (!_items.ContainsKey(hash))
+            {
+                throw new ArgumentException($"Хеш-таблица не содержит элемент с ключом {key}.", nameof(key));
+            }
+
+            var hashTableItem = _items[hash];
+
+            if (hashTableItem != null)
+            {
+                HTElement item = hashTableItem.SingleOrDefault(i => i.Key == key);
+
+                if (item != null)
+                {
+                    return item.Value;
+                }
+            }
+
+            throw new ArgumentException($"Хеш-таблица не содержит элемент с ключом {key}.", nameof(key));
+        }
+
+        private int GetHash(int key)
+        {
+            return key % tableSize;
+        }
+    }
+}
