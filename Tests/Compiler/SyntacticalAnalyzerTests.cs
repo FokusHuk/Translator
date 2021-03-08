20202174f4d9e6d1ed0f1c577d0418ba2b0b1762
Ilya Axenov <31668898+FokusHuk@users.noreply.github.com>
2021-03-08 21:32:53 +0300

    syntactical analyzer tests (with new tests infrastructure)

--- Tests/Compiler/SyntacticalAnalyzerTests.cs
+++ Tests/Compiler/SyntacticalAnalyzerTests.cs
@@ -1,134 +1,191 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using NUnit.Framework;
+﻿using NUnit.Framework;
+using Tests.Infrastructure;
 using Translator.Core.Analyzer;
-using Translator.Core.Lexer;
 
 namespace Tests.Compiler
 {
     public class SyntacticalAnalyzerTests
     {
         [Test]
-        public void Convert_ArithmeticExpressionWithConditions_CorrectTokens()
+        public void Convert_ArithmeticExpression_CorrectPolis()
         {
-            var lexer = new Lexer();
             var syntacticalAnalyzer = new SyntacticalAnalyzer();
-            var expression = @"
-            a = 5; 
-            if(a > 3)
-            { 
-                if(a < 8)
-                {
-                    b = a / 2;
-                } 
-            }
-            c = a + b";
-            var polisExpression = "a 5 = a 3 > 18 !F a 8 < 18 !F b a 2 / = c a b + = $";
-            var tokens = lexer.GetTokensFromExpression(expression);
-            var expected = TestLexer.GetPolisTokensFromExpression(polisExpression);
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.Simple)
+                .Build();
 
-            var actual = syntacticalAnalyzer.Convert(tokens);
-
-            Assert.AreEqual(expected, actual);
-        }
-        
-        [Test]
-        public void Convert_ExpressionWithCyclesAndConditions_CorrectTokens()
-        {
-            var lexer = new Lexer();
-            var syntacticalAnalyzer = new SyntacticalAnalyzer();
-            var expression = @"
-            a = 0;
-            while(a < 10)
-            {
-                a = a + 3;
-                if(a < 7)
-                {
-                    b = 4;
-                }
-                else
-                {
-                    b = 10;
-                } 
-            } 
-            b = b / 2;
-            s = 0;
-            for (i = 1; i <= b; i = i + 1)
-            {
-                s = s + i;
-            }
-            out(a);
-            out(b);
-            out(s);";
-            var polisExpression =
-                "a 0 = a 10 < 28 !F a a 3 + = a 7 < 23 !F b 4 = 26 ! b 10 = 3 ! b b 2 / = s 0 = i 1 = i b <= 60 !F 53" +
-                " ! i i 1 + = 39 ! s s i + = 46 ! a & out b & out s & out $";
-            var tokens = lexer.GetTokensFromExpression(expression);
-            var expected = TestLexer.GetPolisTokensFromExpression(polisExpression);
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
             
-            var actual = syntacticalAnalyzer.Convert(tokens);
-
-            Assert.AreEqual(expected, actual);
+            Assert.AreEqual(program.Polis, actual);
         }
         
         [Test]
-        public void Convert_ExpressionWithFunctions_CorrectTokens()
+        public void Convert_ExpressionWithConditionOnlyIf_CorrectPolis()
         {
-            var lexer = new Lexer();
             var syntacticalAnalyzer = new SyntacticalAnalyzer();
-            var expression = @"
-            a = 5;
-            b = 3;
-            c = sum(a, b);
-            d = upd(c);
-            noArgs();
-    
-            a = 10;
-            out(a);";
-            var polisExpression = "a 5 = b 3 = c a b sum = d c upd = noArgs a 10 = a & out $";
-            var tokens = lexer.GetTokensFromExpression(expression);
-            var expected = TestLexer
-                .GetPolisTokensFromExpression(polisExpression)
-                .Select(token =>
-                {
-                    if (token.Value == "sum" || token.Value == "upd" || token.Value == "noArgs")
-                        return new Token(token.Value, Lexem.EF_NAME);
-                    return token;
-                })
-                .ToList();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.If)
+                .Build();
 
-            var actual = syntacticalAnalyzer.Convert(tokens);
-
-            Assert.AreEqual(expected, actual);
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
         }
         
         [Test]
-        public void Convert_ExpressionWithReturn_CorrectTokens()
+        public void Convert_ExpressionWithConditionIfElse_CorrectPolis()
         {
-            var lexer = new Lexer();
             var syntacticalAnalyzer = new SyntacticalAnalyzer();
-            var expression = @"
-            a = 5;
-            b = 3;
-            return a + b;";
-            var polisExpression = "a 5 = b 3 = a b + return $";
-            var tokens = lexer.GetTokensFromExpression(expression);
-            var expected = TestLexer.GetPolisTokensFromExpression(polisExpression);
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.IfElse)
+                .Build();
 
-            var actual = syntacticalAnalyzer.Convert(tokens);
-
-            Assert.AreEqual(expected, actual);
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
         }
-
-        // TODO: delete this
-        void ForTestCreations(List<Token> tokens)
+        
+        [Test]
+        public void Convert_ExpressionWithNestedConditions_CorrectPolis()
         {
-            var s = "";
-            foreach (var token in tokens)
-            {
-                s += token.Value + " ";
-            }
-            return;
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.NestedConditions)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithCycleWhile_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.CycleWhile)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithCycleWhileAndConditions_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.CycleWhileWithConditions)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithCycleWhileInCondition_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.CycleWhileInCondition)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithCycleFor_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.CycleFor)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithCycleForAndCondition_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.CycleForWithCondition)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithCycleForInCondition_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.CycleForInCondition)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithOutFunction_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.Out)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithOutFunctionInCycles_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.OutInCycles)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithReturn_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.Return)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
+        }
+        
+        [Test]
+        public void Convert_ExpressionWithSeveralReturns_CorrectPolis()
+        {
+            var syntacticalAnalyzer = new SyntacticalAnalyzer();
+            var program = new TestProgramBuilder()
+                .WithSource(TestSourceKey.SeveralReturnsWithFirstWorking)
+                .Build();
+
+            var actual = syntacticalAnalyzer.Convert(program.Tokens);
+            
+            Assert.AreEqual(program.Polis, actual);
         }
     }
 }
